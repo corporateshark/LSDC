f491d8c4c54197cc56dc91e570092763eac92bb0
Sergey Kosarevsky <sk@linderdaum.com>
2017-05-07 03:54:07 +0300

    Removed legacy build configs, use CMake

--- build_t.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import os
-
-if not os.path.exists( "out" ): os.makedirs( "out" )
-
-os.system("make -j4 -B lsdclib -f Makefile.lib")
-os.system("make -j4 -B lsdctest -f Makefile.test")
-os.system("make -j4 -B lsdc -f Makefile.lsdc")
